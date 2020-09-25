c5f2948c54948145497e050af062cba46524780c
cbl <lennart.carbe@gmail.com>
2020-09-25 13:54:34 +0200

    fixed tests

--- tests/migrations/2020_00_00_000000_create_users_table.php
+++ tests/migrations/2020_00_00_000000_create_users_table.php
@@ -15,8 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('email');
-            $table->string('name');
+            $table->string('email')->nullable();
+            $table->string('name')->nullable();
         });
     }
 
